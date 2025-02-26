38d778795f8a4d0da5ebd84483031012d5ed313b
GabrielGL <gabrielunagarcia@hotmail.com>
2025-02-26 17:55:14 -0600

    03 creacion de  my properties(static)

--- app/myproperties/page.tsx
+++ app/myproperties/page.tsx
@@ -3,7 +3,7 @@ const MyPropertiesPage = () =>
     {
     return (
         <main className="max-w-[1500px mx-auto px-6 pb-6" >
-                <h1 className="my-6 mb-6 text-2xl">My Reservations</h1>
+                <h1 className="my-6 mb-6 text-2xl">My Properties</h1>
         </main>        
            )
     }
