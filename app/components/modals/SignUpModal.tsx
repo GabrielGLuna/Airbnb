ad3e7e2037a241e81f29e571c12bee7b9f5e0a69
GabrielGL <gabrielunagarcia@hotmail.com>
2025-03-12 17:02:54 -0600

    12 sign up

--- app/components/modals/SignUpModal.tsx
+++ app/components/modals/SignUpModal.tsx
@@ -1,23 +1,65 @@
 'use client';
 import Modal from "./modal";
 import { useState } from "react"; 
+import { useRouter } from "next/navigation";
 import CustomButton from "../forms/CustomButton";
 import useSignUpModal from "@/app/hooks/useSignUpModal";
+import apiService from "@/app/services/apiService";
+import { handleLogin } from "@/app/lib/actions";
 
 const SignUpModal= ()=>{
-    const signUpmodal = useSignUpModal()
 
+    //variables
+    const router = useRouter();
+    const signUpmodal = useSignUpModal();
+    const [email , setEmail]= useState('');
+    const [password1, setPasword1]= useState('');
+    const [password2, setPasword2]= useState('');
+    const [errors, setErrors] =useState<string[]>([]);
+
+    //submit function
+    const submitSignUp = async ()=>{
+        const formData = {
+            email: email,
+            password1:password1,
+            password2:password2,
+        }
+        const response = await apiService.post('/api/auth/register/',  formData)
+        if(response.access){
+            //handleLogin
+            handleLogin(response.user.pk, response.access, response.refresh)
+            signUpmodal.close();
+            router.push('/')
+        }else{
+            const tmpErrors: string[] = Object.values(response).map((error:any)=>{
+                return error
+            })
+            setErrors(tmpErrors);
+        }
+    }
+    
     const content= (   
         <>
              <h2 className="mb-6 text-2xl">Welcome to DjangoBNB, please Login</h2>
-             <form action="" className="space-y-4">
-                <input type="email" className="w-full h-[54px] border px-4 border-gray-300 rounded-xl" placeholder="Your email address" />
-                <input type="password" className="w-full h-[54px] border px-4 border-gray-300 rounded-xl" placeholder="Your password" />
-                <input type="password" className="w-full h-[54px] border px-4 border-gray-300 rounded-xl" placeholder="Repeat your password" />
-                <div className="p-5 bg-airbnb text-white rounded-xl opacity-60">The error message</div>
+             <form 
+                onSubmit={submitSignUp}
+                className="space-y-4">
+                <input onChange={(e) => setEmail(e.target.value)} type="email" className="w-full h-[54px] border px-4 border-gray-300 rounded-xl" placeholder="Your email address" />
+                <input onChange={(e) => setPasword1(e.target.value)} type="password" className="w-full h-[54px] border px-4 border-gray-300 rounded-xl" placeholder="Your password" />
+                <input onChange={(e) => setPasword2(e.target.value)} type="password" className="w-full h-[54px] border px-4 border-gray-300 rounded-xl" placeholder="Repeat your password" />
+                {errors.map((error, index) => {
+                    return (
+                        <div 
+                            key={`error_${index}`}  // Aquí se usa el valor de index correctamente
+                            className="p-5 bg-airbnb text-white rounded-xl opacity-60">
+                            {error}
+                        </div>
+                    );
+                })}
+
                 <CustomButton
                  label="submit"
-                 onclick={()=> console.log("Hola")}
+                 onclick={submitSignUp}
                 />
              </form>
 
--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -1,24 +1,48 @@
 const apiService = {
-    get: async function(url:string): Promise<any>{
-        console.log('get', url)
-        return new Promise((resolve, reject) =>{
-            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`,{
-                method:'GET',
-                headers:{
-                    'Accept' : 'application/json',
-                    'Content-Type' : 'application/json'
+    get: async function(url: string): Promise<any> {
+        console.log("API HOST en tiempo de ejecución:", process.env.NEXT_PUBLIC_API_HOST);
+        console.log('get', url);
+        return new Promise((resolve, reject) => {
+            console.log("API HOST:", process.env.NEXT_PUBLIC_API_HOST);
+
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
                 }
             })
-            .then(response => response.json())
-            .then((json)=>{
-                console.log('Response', json);
-
-                resolve(json);
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('Response', json);
+                    resolve(json);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    },
+    post: async function(url: string, data: any): Promise<any> {
+        console.log('post', url, data);
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
             })
-            .catch((error =>{
-                reject(error)
-            }))
-        })
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('Response', json);
+                    resolve(json);
+                })
+                .catch((error) => {
+                    console.error('Error en Post:', error);
+                    reject(error);
+                });
+        });
     }
-}
+};
 export default apiService;
