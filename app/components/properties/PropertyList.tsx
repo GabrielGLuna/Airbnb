65948104ac0bcaf13f69cdf252721047d7f1dd5b
GabrielGL <gabrielunagarcia@hotmail.com>
2025-03-12 13:41:55 -0600

    11 create api service

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,9 +1,8 @@
 'use client';
 
+import apiService from "@/app/services/apiService";
 import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem"
-import { json } from "stream/consumers";
-import { error } from "console";
 
 export type PropertyType = {
     id:string;
@@ -15,20 +14,14 @@ const PropertyList =()=>{
     const [properties, setProperties] = useState<PropertyType[]>([]);
 
     const getProperties = async () =>{
-        const url = 'http://localhost:8000/api/properties';
-        await fetch(url,{
-            method: 'GET',
-        })
-        .then(response => response.json())
-        .then((json) =>{
-            console.log('json',json)
-            setProperties(json.data)
-        })
-        .catch((error) =>{
-            console.log('error', error)
-        })
+        const tmpProperties = await apiService.get('/api/properties/')
+
+        setProperties(tmpProperties.data)
+
     };
+        
     useEffect(()=>{
+            
             getProperties();
     }, []) 
     
